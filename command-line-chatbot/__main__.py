2c58b8db9083c2a4e1480db91bdf8bafa02f10e4
RowanGray472 <rgray26@cmc.edu>
2024-11-15 18:59:21 -0800

    adding files we need to code! templating stuff

---
+++ command-line-chatbot/__main__.py
@@ -0,0 +1,7 @@
+"""
+Stuff that will go in this file
+
+All the rag stuff
+
+All the chatbot interface stuff (except install stuff)
+"""
---
+++ setup.sh
@@ -0,0 +1,4 @@
+# This file will contain all of our setup.
+# Specifically-
+# Code that creates the database automatically based on your man pages
+# Code that installs a llama model and gets it ready to go on your computer
