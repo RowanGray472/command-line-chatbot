c1c381c142362b58554c2bb5c92d17982f2dfb2e
RowanGray472 <rgray26@cmc.edu>
2024-11-21 20:24:27 -0800

    combining setup stuff

--- setup.sh
+++ setup.sh
@@ -11,4 +11,14 @@ ollama pull llama3.2
 
 # database setup
 
+OUT_DIR=~/manpages
 
+rm -rf $OUT_DIR
+mkdir $OUT_DIR
+for i in `find /usr/share/man -name '*.gz'`; do
+    dname=$(dirname $i)
+    mkdir -p "$OUT_DIR/$dname"
+    zcat "$i" | soelim | tbl | groff -mandoc -Tutf8 | col -b > "$OUT_DIR/$i.txt"
+done
+
+python3 manpages_to_sqlite.py
